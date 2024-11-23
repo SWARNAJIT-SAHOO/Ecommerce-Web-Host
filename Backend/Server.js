241796ee0d2c21a541e8a2f331c6eabab8da09d9
Swarnajit Sahoo <123718373+SWARNAJIT-SAHOO@users.noreply.github.com>
2024-11-23 19:22:21 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,6 +10,8 @@ const secretKey = 'swarnajit01';
 
 
 app.use(cors({ origin: 'https://repo1-rose.vercel.app/', 
+              methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+              credentials: true
 
 
 mongoose.connect('mongodb+srv://swarnajit:aMF9PuWr9YgcTQA9@cluster0.94qfb.mongodb.net/Ecommerce', {
